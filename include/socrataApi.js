8fe648cfe035e47579322422c2736748f9d708dd
Ethan <epchow@hawaii.edu>
2017-09-16 16:42:58 -1000

    Started socrataAPI module

---
+++ include/socrataApi.js
@@ -0,0 +1,9 @@
+let projectIdMapping = {
+	// w6ex-izbf is a duplicate, but missing one field
+	"petroleum-cost-per-person": "5x4d-ggyg",
+	"total-energy-primary": "yt6g-n3gy"
+};
+
+module.exports = {
+	
+};
